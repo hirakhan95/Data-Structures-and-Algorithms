a2262583f147279f5d82cf0932a8e791dbf5b3db
Hira Khan <phr.hirakhan@gmail.com>
2023-10-28 21:38:09 +0200

    pop function completed

--- EXERCISE-LL-Pop.py
+++ EXERCISE-LL-Pop.py
@@ -1,10 +1,17 @@
 class Node:
-    def __init__(self, value):
+    value: any
+    next: any
+
+    def __init__(self, value: any):
         self.value = value
         self.next = None
         
 
 class LinkedList:
+    head: Node
+    tail: Node
+    length: int = 1
+
     def __init__(self, value):
         new_node = Node(value)
         self.head = new_node
@@ -28,29 +35,30 @@ class LinkedList:
         self.length += 1
         return True
 
-    def pop(self,value):
+    def pop(self):
         if self.length == 0:
             return None
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
 
- 
 
-
-my_linked_list = LinkedList(1)
-my_linked_list.append(2)
+my_linked_list = LinkedList(0)
+for i in range(1, 10):
+    my_linked_list.append(i)
 
 # (2) Items - Returns 2 Node
-print(my_linked_list.pop().value)
-# (1) Item -  Returns 1 Node
-print(my_linked_list.pop().value)
-# (0) Items - Returns None
-print(my_linked_list.pop())
 
-
-"""
-    EXPECTED OUTPUT:
-    ----------------
-    2
-    1
-    None
-
-"""
+node = my_linked_list.pop()
+while node:
+    print(node.value)
+    node = my_linked_list.pop()
